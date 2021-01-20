bb4ba3e39c1545273dce89b82599c9b549fd62b7
Melkin Mosquera <melanmosta@gmail.com>
2021-01-20 00:04:35 -0500

    task#6: concatenate matrices

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays
+    Args:
+        arr1 ([int]): a list of elements
+        arr2 ([int]): a list of elements
+    """
+    new_list = arr1 + arr2
+    return new_list
