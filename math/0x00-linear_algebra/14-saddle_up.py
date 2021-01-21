a3509bca189a649b32416d444391f75d0a7de978
Melkin Mosquera <melanmosta@gmail.com>
2021-01-21 17:06:23 -0500

    task#14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""multiply matrices"""
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication
+
+    Args:
+        mat1 ([int]): a list of elements
+        mat2 ([int]): a list of elements
+    """
+    return mat1.dot(mat2)
