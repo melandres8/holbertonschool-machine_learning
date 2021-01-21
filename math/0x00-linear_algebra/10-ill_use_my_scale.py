44b2b9393044e112ace2f1e13635e420ebf53566
Melkin Mosquera <melanmosta@gmail.com>
2021-01-21 16:38:40 -0500

    numpy shape

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""np_shape function"""
+
+
+def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray.
+
+    Args:
+        matrix ([int]): a list of elements
+    """
+    shape = matrix.shape
+    return shape
