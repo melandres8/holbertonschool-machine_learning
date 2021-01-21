0681f86284447794d9c4efac49b484c6fed2ea1a
Melkin Mosquera <melanmosta@gmail.com>
2021-01-21 16:42:22 -0500

    transpose

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""transpose function"""
+
+
+def np_transpose(matrix):
+    """Transposes a matrix
+
+    Args:
+        matrix ([int]): a list of elements
+    """
+    transpose = matrix.transpose()
+    return transpose
