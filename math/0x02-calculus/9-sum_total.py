2270c73282cdf2ed57c34144e5f025836153e749
Melkin Mosquera <melanmosta@gmail.com>
2021-01-27 22:42:12 -0500

    sigma

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """Summation"""
     if n > 0 and isinstance(n, int):
-        return n ** 2 * 2 + 5
+        return int(((n ** 2 * 2 + 5) * 2) / 2)
     else:
         return None
