578dfbcb0a21506da839d7c9bf3a017efaa1a2c4
Melkin Mosquera <melanmosta@gmail.com>
2021-01-28 23:01:33 -0500

    polynomial derivative

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,7 +4,9 @@
 
 def poly_derivative(poly):
     """Derivative func"""
-    if poly == 0:
+    if poly == 1:
         return [0]
+    elif not isinstance(poly, list) or len(poly) == 0:
+        return None
     else:
         return [poly[i] * i for i in range(1, len(poly))]
