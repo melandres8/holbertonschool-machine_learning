7a707a0d1a1ca6ea636721bd2dc7f2683895a029
Melkin Mosquera <melanmosta@gmail.com>
2021-02-03 12:10:41 -0500

    initialize normal requirements fixed

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -7,14 +7,14 @@ class Normal:
     def __init__(self, data=None, mean=0., stddev=1.):
         """Constructor"""
         if data is None:
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
             self.stddev = float(stddev)
             self.mean = float(mean)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.mean = float(sum(data) / len(data))
             summa = [(x - self.mean) ** 2 for x in data]
