62553705bae73d144e3f1b9812a8d3869bf6d427
Melkin Mosquera <melanmosta@gmail.com>
2021-02-03 13:02:34 -0500

    PDF normal

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -29,3 +29,9 @@ class Normal:
         """Calculates the x-value
         of a given z-score"""
         return self.mean + z * self.stddev
+
+    def pdf(self, x):
+        """Calculates the value of the
+        PDF for a given x-value"""
+        return 1 / (self.stddev * (2 * 3.1415926536) ** 0.5) * \
+            2.7182818285 ** (-(x - self.mean) ** 2 / (2 * self.stddev ** 2))
