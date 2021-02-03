cf9ecf012d57eb1e5c57850b0e8bab8101df312f
Melkin Mosquera <melanmosta@gmail.com>
2021-02-03 11:21:45 -0500

    pdf function

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -16,3 +16,17 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / float(sum(data) / len(data))
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF
+        for a given time period
+        """
+        if not isinstance(x, int):
+            x = int(x)
+        if x <= 0:
+            return 0
+
+        result = self.lambtha * 2.7182818285 ** (-self.lambtha * x)
+
+        return result
