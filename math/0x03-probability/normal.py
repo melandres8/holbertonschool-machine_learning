09ee997e4b134ab8872a9348c8bc2385743bb1be
Melkin Mosquera <melanmosta@gmail.com>
2021-02-03 12:41:21 -0500

    z-score and x-value calculate

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -19,3 +19,13 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             summa = [(x - self.mean) ** 2 for x in data]
             self.stddev = pow((sum(summa) / len(data)), 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score
+        of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value
+        of a given z-score"""
+        return self.mean + z * self.stddev
