09048c450e5f134dbc327326e74d6bc2497b6c2a
Melkin Mosquera <melanmosta@gmail.com>
2021-02-16 20:56:55 -0500

    improving conditionals

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -10,11 +10,11 @@ class NeuralNetwork:
         """Constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx <= 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
-        if nodes <= 1:
+        if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         self.W1 = np.random.normal(0, 1, (nodes, nx))
         self.b1 = np.zeros((nodes, 1))
