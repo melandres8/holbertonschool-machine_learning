37c0c0100ee293f6bcadc81891d67f1f39c66914
Melkin Mosquera <melanmosta@gmail.com>
2021-02-16 22:45:00 -0500

    training net

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -112,7 +112,7 @@ class NeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        for i in range(iterations):
+        for _ in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
         return self.evaluate(X, Y)
