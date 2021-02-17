fed9b8f0ee37cb8b7abf04d2fd5c4be65c94c83f
Melkin Mosquera <melanmosta@gmail.com>
2021-02-16 19:29:53 -0500

    first training

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -82,7 +82,7 @@ class Neuron:
         if alpha <= 0:
             raise ValueError("alpha must be positive")
 
-        for i in range(iterations):
+        for _ in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A, alpha)
 
